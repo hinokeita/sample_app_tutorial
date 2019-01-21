3625e769014565c1bf8b99ec5b61d1db0cbf97e0
hinokeita <hino@sackle.co.jp>
2019-01-21 06:06:26 +0000

    add:テストケース追加

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,24 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  before do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  # エラー時の分かりやすさより速度を求めるならitの中にタイトルのテストも入れる。
   describe '#home' do
-    it 'should get home' do
+    before do
       get :home
+    end
+
+    it 'should get home' do
       expect(response).to have_http_status(:success)
     end
+
+    it 'title correct' do
+      assert_select "title", "Home | #{@base_title}"
+    end
+
   end
 
   describe '#help' do
-    it 'should get help' do
+    before do
       get :help
+    end
+
+    it 'should get help' do
       expect(response).to have_http_status(:success)
     end
+
+    it 'title correct' do
+      assert_select "title", "Help | #{@base_title}"
+    end
+
   end
 
   describe '#about' do
-    it 'should get about' do
+    before do
       get :about
+    end
+
+    it 'should get about' do
       expect(response).to have_http_status(:success)
     end
+
+    it 'title correct' do
+      assert_select "title", "About | #{@base_title}"
+    end
+
   end
+
+  describe '#contact' do
+    before do
+      get :contact
+    end
+
+    it 'should get contact' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'title correct' do
+      assert_select "title", "Contact | #{@base_title}"
+    end
+  end
+
 end
