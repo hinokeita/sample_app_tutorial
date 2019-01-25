9fdbe765063a421ff25cec167fed2c17cbe5784d
hinokeita <hino@sackle.co.jp>
2019-01-25 04:50:57 +0000

    add:ルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'user/new'
 
   root 'static_pages#home'
   get '/static_pages/home', to: 'static_pages#home'
   get '/static_pages/help', to: 'static_pages#help'
   get '/static_pages/about', to: 'static_pages#about'
   get '/static_pages/contact', to: 'static_pages#contact'
-  get '/signup', to: 'user#new'
+  get '/signup', to: 'users#new'
+  resources :users
+
 end
