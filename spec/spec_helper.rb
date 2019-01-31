40a4682994564bf893c6e90e8d2974020ea80868
hinokeita <hino@sackle.co.jp>
2019-01-31 01:10:43 +0000

    add:capybaraの設定追加

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
-
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'capybara/rspec'
+require 'capybara/rails'
+include Capybara::DSL
+ 
 RSpec.configure do |config|
+  config.include Rails.application.routes.url_helpers
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
