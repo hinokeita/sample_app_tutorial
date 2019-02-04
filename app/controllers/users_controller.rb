97001ebcd3d5fec4306360d1dcfd45d3588c2489
hinokeita <hino@sackle.co.jp>
2019-02-04 02:57:31 +0000

    modify:createメソッド修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,9 +24,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample APP!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activatate token account."
+      redirect_to root_url
     else
       render 'new'
     end
