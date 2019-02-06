7f0fcebb64a33c91f6be992aa95764d1d0f9e206
hinokeita <hino@sackle.co.jp>
2019-02-06 07:31:06 +0000

    refactor:logged_in_userメソッドをApplicationControllerに移動

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
@@ -51,17 +52,9 @@ class UsersController < ApplicationController
      params.require(:user).permit(:name,:email,:password,:password_confirmation)
    end
 
-   def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in"
-        redirect_to login_url
-      end
-   end
-
    def correct_user
      @user = User.find(params[:id])
-     redirect_to(root_url) unless correct_user?(@user)
+     redirect_to(root_url) unless current_user?(@user)
    end
 
    def admin_user
